d5eb26f9aca6b761931192b96c35fe0c211e0e34
soilforlifeforms <rvermooten@gmail.com>
2014-05-31 02:55:57 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Formit::Application.config.secret_key_base = '9843c85ec1cd069fd896439c11fa11c5c6cfc1dd59fc5ccc99fbe78649eddd9c30a4ede065b1f0424479371f9d2b4d894afee56419117961e12a635964941618'
